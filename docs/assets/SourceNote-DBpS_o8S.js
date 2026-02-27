d4a5f0bf2acc9481f3a471fc7f62a0faa3c47ab4
WonhongYoo <wh.y@navercorp.com>
2026-02-27 14:30:00 +0900

    fix

---
+++ docs/assets/SourceNote-DBpS_o8S.js
@@ -0,0 +1,1 @@
+import{f as e,o,e as s,t as n}from"./modules/vue-dBzYZe2V.js";import{u as _}from"./slidev/context-CPEH-5R9.js";import{_ as r}from"./index-DgVgD2ir.js";const c={class:"source-note"},a=e({__name:"SourceNote",props:{text:{}},setup(t){return _(),(p,i)=>(o(),s("div",c,"참고: "+n(t.text),1))}}),f=r(a,[["__scopeId","data-v-b398d437"]]);export{f as _};
